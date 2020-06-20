0e0ab8c2cf10aedf49eb0a58b5807a07383fa071
Herhos <herhos@outlook.com>
2020-06-20 15:57:10 -0300

    Obtendo um usuário por id

--- src/main/java/com/herhos/workshopmongo/services/UserService.java
+++ src/main/java/com/herhos/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.herhos.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.herhos.workshopmongo.domains.User;
 import com.herhos.workshopmongo.repositories.UserRepository;
+import com.herhos.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService
@@ -17,5 +19,11 @@ public class UserService
 	public List<User> findAll()
 	{
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id)
+	{
+		Optional <User> obj= userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
 }
