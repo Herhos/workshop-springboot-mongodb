7f6ff36bcf6dff7e340313c5ee0af3ee14a49d9a
Herhos <herhos@outlook.com>
2020-06-21 17:06:02 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/herhos/workshopmongo/domains/Post.java
+++ src/main/java/com/herhos/workshopmongo/domains/Post.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.herhos.workshopmongo.dtos.AuthorDto;
+
 @Document
 public class Post implements Serializable
 {	
@@ -15,14 +17,14 @@ public class Post implements Serializable
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDto author;
 	
 	public Post()
 	{
 		super();
 	}
 
-	public Post(String id, Date date, String title, String body, User author)
+	public Post(String id, Date date, String title, String body, AuthorDto author)
 	{
 		super();
 		this.id = id;
@@ -72,12 +74,12 @@ public class Post implements Serializable
 		this.body = body;
 	}
 	
-	public User getAuthor()
+	public AuthorDto getAuthor()
 	{
 		return author;
 	}
 
-	public void setAuthor(User author)
+	public void setAuthor(AuthorDto author)
 	{
 		this.author = author;
 	}
