d564179b35d706d84d3a388bbd49151e628637d3
Herhos <herhos@outlook.com>
2020-06-21 14:03:02 -0300

    Atualização de usuário com PUT

--- src/main/java/com/herhos/workshopmongo/services/UserService.java
+++ src/main/java/com/herhos/workshopmongo/services/UserService.java
@@ -39,8 +39,21 @@ public class UserService
 		userRepository.deleteById(id);
 	}
 	
+	public User update(User obj)
+	{
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return userRepository.save(newObj);
+	}
+	
 	public User fromDto(UserDto objDto)
 	{
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail()); 
-	}	
+	}
+	
+	public void updateData(User newObj, User obj)
+	{
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
 }
