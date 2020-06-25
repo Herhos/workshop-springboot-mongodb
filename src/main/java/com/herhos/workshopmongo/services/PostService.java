9de45d89d40149d0e5792fdac09fb5f87b979c3f
Herhos <herhos@outlook.com>
2020-06-25 16:34:09 -0300

    Obtendo um post por id

---
+++ src/main/java/com/herhos/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.herhos.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.herhos.workshopmongo.domains.Post;
+import com.herhos.workshopmongo.repositories.PostRepository;
+import com.herhos.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService
+{
+	@Autowired
+	public PostRepository postRepository;
+	
+	public Post findById(String id)
+	{
+		Optional <Post> obj= postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}	
+}
