770c74eeb62350bb4d16641bdb3554e8c49d1646
Herhos <herhos@outlook.com>
2020-06-26 16:33:45 -0300

    Consulta simples com @Query

--- src/main/java/com/herhos/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/herhos/workshopmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.herhos.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.herhos.workshopmongo.domains.Post;
@@ -11,4 +12,10 @@ import com.herhos.workshopmongo.domains.Post;
 public interface PostRepository extends MongoRepository<Post, String>
 {
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	// 'title' é o campo utilizado para a busca
+	// ?0 é o primeiro parâmetro do método
+	// 'i' indica busca case insensitive
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 }
